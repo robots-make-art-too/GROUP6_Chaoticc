81e1267b27ab223b7b5dd445dda956d81b443ab7
dawegw2 <90479136+dawegw2@users.noreply.github.com>
2022-04-13 12:44:20 -0700

    Update script.js

--- docs/website/script.js
+++ docs/website/script.js
@@ -4,6 +4,13 @@ window.onload = () => {
 };
 
 var models = [ //array of the planets and their attributes
+    {    
+        url: 'assets/models/sun.gltf',
+        scale: '0.5 0.5 0.5',
+        name: 'The Sun',
+        value: "The Sun is the center of our solar system. From planets to astroids, everything in our solar system revolves around the Sun. Being the only star in our solar system, the Sun is an important source for life on Earth.",
+        position: "-0.90 0 0.90",
+    },
     {
         url: 'assets/models/mercury.gltf',
         scale: '0.5 0.5 0.5',
@@ -123,6 +130,3 @@ function hideDescription() { //hides/shows planet description
         description.setAttribute('value', models[modelIndex-1].value)
     }
 }
-
-
-
